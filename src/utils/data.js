4acc1aaaca87ef6e509a1041a1717ed8487d8e91
Alexander_Ovchar <azalexxx02@gmail.com>
2024-02-08 19:03:42 +0200

    changeData

--- src/utils/data.js
+++ src/utils/data.js
@@ -3,7 +3,6 @@ export const folders = [
     id: 1000,
     name: "Folder 1",
     filesId: [1, 2, 3],
-
     type: "folder",
   },
   {
@@ -25,100 +24,120 @@ export const files = [
     id: 1,
     name: "File 1",
     size: "1024", // in bytes
+    type: "file",
   },
   {
     id: 2,
     name: "File 2",
     size: "2048", // in bytes
+    type: "file",
   },
   {
     id: 3,
     name: "File 3",
     size: "4096", // in bytes
+    type: "file",
   },
   {
     id: 4,
     name: "File 4",
     size: "8192", // in bytes
+    type: "file",
   },
   {
     id: 5,
     name: "File 5",
     size: "16384", // in bytes
+    type: "file",
   },
   {
     id: 6,
     name: "File 6",
     size: "32768", // in bytes
+    type: "file",
   },
   {
     id: 7,
     name: "File 7",
     size: "65536", // in bytes
+    type: "file",
   },
   {
     id: 8,
     name: "File 8",
     size: "131072", // in bytes
+    type: "file",
   },
   {
     id: 9,
     name: "File 9",
     size: "262144", // in bytes
+    type: "file",
   },
   {
     id: 10,
     name: "File 10",
     size: "524288", // in bytes
+    type: "file",
   },
   {
     id: 11,
     name: "File 11",
     size: "1048576", // in bytes
+    type: "file",
   },
   {
     id: 12,
     name: "File 12",
     size: "2097152", // in bytes
+    type: "file",
   },
   {
     id: 13,
     name: "File 13",
     size: "4194304", // in bytes
+    type: "file",
   },
   {
     id: 14,
     name: "File 14",
     size: "8388608", // in bytes
+    type: "file",
   },
   {
     id: 15,
     name: "File 15",
     size: "16777216", // in bytes
+    type: "file",
   },
   {
     id: 16,
     name: "File 16",
     size: "33554432", // in bytes
+    type: "file",
   },
   {
     id: 17,
     name: "File 17",
     size: "67108864", // in bytes
+    type: "file",
   },
   {
     id: 18,
     name: "File 18",
     size: "134217728", // in bytes
+    type: "file",
   },
   {
     id: 19,
     name: "File 19",
     size: "268435456", // in bytes
+    type: "file",
   },
   {
     id: 20,
     name: "File 20",
     size: "536870912", // in bytes
+    type: "file",
   },
 ];
